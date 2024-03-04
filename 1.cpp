52e9b24c10c44b66fbb97cab8c040fa7a8b14109
Hakan ツ <41235163+cafune34@users.noreply.github.com>
2024-03-05 00:13:36 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sayi1, sayi2, toplam;
+    cout << "iki adet sayi giriniz\n";
+    cin >> sayi1;
+    cin >> sayi2;
+    toplam = sayi1 + sayi2;
+    cout << "toplam: " << toplam;
+    return 0;
+}
