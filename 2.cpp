3ab683fab3c61969fa37647d3edad2a1907c9bd3
Hakan ツ <41235163+cafune34@users.noreply.github.com>
2024-03-06 11:36:18 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x = 14.74, y = 9.36, toplam;
+    toplam = (x+y)+(x-y)+(x*y);
+    cout << "toplam: " << toplam;
+    return 0;
+}
